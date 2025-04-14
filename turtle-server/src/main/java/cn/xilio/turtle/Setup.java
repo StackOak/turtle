8811cdaef7c808f2cb0732ae948647d8d64d0062
xilio <termitetech@163.com>
2025-04-14 18:55:37 +0800

    style: 新增注释

--- turtle-server/src/main/java/cn/xilio/turtle/Setup.java
+++ turtle-server/src/main/java/cn/xilio/turtle/Setup.java
@@ -2,7 +2,15 @@ package cn.xilio.turtle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(scanBasePackages = {"cn.xilio.turtle","com.baidu.fsg.uid"})
+
+/**
+ * @Project Turtle
+ * @Description 项目启动类
+ * @Author xilio
+ * @Website https://xilio.cn
+ * @Copyright (c) 2025 xilio. All Rights Reserved.
+ */
+@SpringBootApplication(scanBasePackages = {"cn.xilio.turtle", "com.baidu.fsg.uid"})
 public class Setup {
 
     public static void main(String[] args) {
