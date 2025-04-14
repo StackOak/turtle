4b267927c6d4db99adca60aad167ebc0d97bb51e
xilio <termitetech@163.com>
2025-04-14 16:01:59 +0800

    refactor: 优化文件上传路径，如果用户没有配置则根据不同操作系统设置默认路径

--- turtle-server/src/main/java/cn/xilio/turtle/config/ResourcesConfig.java
+++ turtle-server/src/main/java/cn/xilio/turtle/config/ResourcesConfig.java
@@ -1,17 +1,18 @@
 package cn.xilio.turtle.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class ResourcesConfig implements WebFluxConfigurer {
-    @Value("${cn.xilio.turtle.upload.path}")
-    private String uploadPath;
+    private final TurtleProperties properties;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/oss/file/**")
-                .addResourceLocations("file:/" + uploadPath + "/");
+                .addResourceLocations("file:/" + properties.getUpload().getPath() + "/");
     }
 }
--- turtle-server/src/main/java/cn/xilio/turtle/config/TurtleProperties.java
+++ turtle-server/src/main/java/cn/xilio/turtle/config/TurtleProperties.java
@@ -1,12 +1,23 @@
 package cn.xilio.turtle.config;
 
+import cn.xilio.turtle.utils.OSUtils;
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.NotEmpty;
+
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 
-@Data
+import java.nio.file.Paths;
+
+import static cn.xilio.turtle.utils.OSUtils.OSType.*;
+
+@Getter
+@Setter
 @Configuration
 @Validated
 @ConfigurationProperties(prefix = "cn.xilio.turtle")
@@ -15,22 +26,45 @@ public class TurtleProperties {
     private final Upload upload = new Upload();
     private final Api api = new Api();
 
-    @Data
+    @PostConstruct
+    public void initUploadPath() {
+        if (!StringUtils.hasText(upload.getPath())) {
+            upload.setPath(getOSDefaultUploadPath());
+        }
+    }
+
+    /**
+     * 如果用户没有配置文件上传路径，则根据不同的操作系统设置默认路径
+     */
+    private static String getOSDefaultUploadPath() {
+        String userHome = System.getProperty("user.home");
+        return switch (OSUtils.getOSType()) {
+            case WINDOWS -> Paths.get(userHome, "Desktop", "turtle_uploads").toString();
+            case MACOS -> Paths.get(userHome, "Documents", "turtle_uploads").toString();
+            case LINUX -> "/var/tmp/turtle_uploads";
+            default -> "./upload"; // 其他系统使用当前目录
+        };
+    }
+
+    @Getter
+    @Setter
     public static class Upload {
         /**
          * 文件上传基础路径
          */
         @NotEmpty(message = "文件上传路径前缀不能为空")
-        private String path = "./upload";
+        private String path;
 
         /**
          * 允许的文件类型，空表示不限制
          */
         private String[] allowedExtensions = {"jpg", "png", "gif", "pdf", "doc", "docx"};
+
+
     }
 
-
-    @Data
+    @Setter
+    @Getter
     public static class Api {
         /**
          * API前缀
