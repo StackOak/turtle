750c72ffa8b3c194cfc981d5c4c03c4c6d793e10
xilio <termitetech@163.com>
2025-04-17 01:10:48 +0800

    feat: 知识库编辑 增加新增文件夹和文档处理

--- turtle-site/pages/console/book/[id].vue
+++ turtle-site/pages/console/book/[id].vue
@@ -99,13 +99,27 @@ const items2 = ref<DropdownMenuItem[]>([
         icon: 'i-material-symbols-markdown-rounded',
         children: [], // 确保新节点是叶子节点
       };
-      bookItems.value=[...bookItems.value,{label:`new-${Date.now()}`,icon: 'i-vscode-icons-file-type-nuxt'}]
+      bookItems.value = [...bookItems.value,
+        {label: `new-${Date.now()}`, icon: 'i-material-symbols-markdown-outline-sharp',children:[]}]
 
     }
   },
   {
     label: '新增目录',
-    icon: 'i-material-symbols-folder-rounded'
+    icon: 'i-material-symbols-folder-rounded',
+    onSelect(e: Event) {
+
+      // 添加一个新的文档节点
+      const newItem = {
+        id: `new-${Date.now()}`, // 临时生成唯一 ID
+        label: '新建文档',
+        icon: 'i-material-symbols-markdown-rounded',
+        children: [], // 确保新节点是叶子节点
+      };
+      bookItems.value = [...bookItems.value,
+        {label: `new-${Date.now()}`, icon: 'i-akar-icons-folder',children:[]}]
+
+    }
   },
   {
     label: '重命名',
@@ -120,10 +134,10 @@ const items2 = ref<DropdownMenuItem[]>([
 </script>
 
 <template>
-  <div class="shadow-[0_2px_3px_-1px_rgba(0,0,0,0.1)] sticky top-0 self-start z-500">
+  <div class="shadow-[0_2px_3px_-1px_rgba(0,0,0,0.1)]  z-500">
     <ReBack class="pl-4" title=" 大数据开发"/>
   </div>
-  <div class="flex flex-row pl-2 h-[calc(100vh-64px)]">
+  <div class="flex flex-row pl-2 h-[calc(100vh-64px)] fixed">
     <!-- 左侧菜单 -->
     <div class="w-auto max-w-xs z-10 overflow-y-auto max-h-[calc(100vh-64px)]">
       <!-- 侧边栏 -->
@@ -131,9 +145,9 @@ const items2 = ref<DropdownMenuItem[]>([
         <!-- 目录和操作 -->
         <div class="flex flex-row justify-between items-center px-4">
           <span class="text-sm font-medium text-gray-600">目录</span>
-          <UDropdownMenu   :items="items2" trigger="hover" placement="bottom-end">
+          <UDropdownMenu :items="items2" trigger="hover" placement="bottom-end">
             <UButton variant="ghost" size="sm" class="p-1">
-              <UIcon name="i-famicons-add-sharp" class="w-5 h-5 text-gray-500" />
+              <UIcon name="i-famicons-add-sharp" class="w-5 h-5 text-gray-500"/>
             </UButton>
           </UDropdownMenu>
         </div>
@@ -143,7 +157,7 @@ const items2 = ref<DropdownMenuItem[]>([
           <UTree
               :ui="{link:'cursor-pointer'}"
               v-model="selectedItem"
-               v-model:items="bookItems"
+              v-model:items="bookItems"
               size="xl"
               class="min-w-[200px]"
           />
@@ -152,35 +166,22 @@ const items2 = ref<DropdownMenuItem[]>([
     </div>
     <USeparator orientation="vertical" class="h-auto min-h-[calc(100vh-64px)]"/>
     <!-- 右侧编辑器 -->
-    <div class="flex-1 p-4 overflow-y-auto max-h-[calc(100vh-64px)]">
+    <div class="flex-1   max-h-[calc(100vh-64px)]">
       <CherryEditor
           ref="editorRef"
+          :height="95"
           v-model="content"
           code-theme="dark"
           main-theme="default"
           anchor-style="none"
           id="editor-main"
-          :preview="true"
+          :preview="false"
       />
     </div>
   </div>
 </template>
 
 <style scoped>
-:deep(.cherry-previewer) {
-  border-left: none;
-  padding: 0;
-}
-
-:deep(.cherry-mask-code-block .expand-btn) {
-  color: white;
-  background-color: rgb(43, 43, 43);
-}
-
-:deep(.cherry-mask-code-block .expand-btn):hover {
-  background-color: black;
-}
-
 /* 确保左右区域独立滚动 */
 .flex-row {
   overscroll-behavior: contain; /* 防止滚动穿透 */
