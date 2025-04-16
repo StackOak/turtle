a9e8c036436f8d937a2e5472e298bc11a80ea8cd
xilio <termitetech@163.com>
2025-04-16 11:46:00 +0800

    perf: 优化门户菜单列表 采用动态菜单 支持SSR

---
+++ turtle-site/server/api/menu/left-menus.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+    const config = useRuntimeConfig()
+    const query = getQuery(event)
+    const res= await <any>$fetch(`${config.public.userApiBase}/left-menus`, {
+        method: 'GET',
+        query
+    })
+    return res.data
+})
