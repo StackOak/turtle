dc8b4156afbc9700c20aab3341afd2bff99c9614
xilio <termitetech@163.com>
2025-04-17 23:27:47 +0800

    fix:修复跨域问题

--- turtle-server/src/main/java/cn/xilio/turtle/config/WebConfig.java
+++ turtle-server/src/main/java/cn/xilio/turtle/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
