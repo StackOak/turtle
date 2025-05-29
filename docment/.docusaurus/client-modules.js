4feea4e6825a0853cdf4bd715c02746091a40223
xilio <xilio1024@gmail.com>
2025-05-29 11:45:10 +0800

    feat: 删除没用的文件

--- docment/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/liuxin/Desktop/turtle/docment/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/liuxin/Desktop/turtle/docment/node_modules/.pnpm/@docusaurus+theme-classic@3.7.0_@types+react@19.1.2_acorn@8.14.1_react-dom@19.1.0_react_95dddf9a20f77ced4eb53b92315cc3c1/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/liuxin/Desktop/turtle/docment/node_modules/.pnpm/@docusaurus+theme-classic@3.7.0_@types+react@19.1.2_acorn@8.14.1_react-dom@19.1.0_react_95dddf9a20f77ced4eb53b92315cc3c1/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/liuxin/Desktop/turtle/docment/src/css/custom.css"),
-];
